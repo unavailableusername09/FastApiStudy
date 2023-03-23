3ec48b2d15c0ec4d18213fefe1af99197b018650
unavailableusername09 <legeno09133@gmail.com>
2023-03-23 18:09:51 +0900

    create question

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
+from starlette import status
 
 from domain.question import question_schema,  question_service
 from database import get_db
@@ -20,3 +20,9 @@ def question_list(db: Session = Depends(get_db)):
 def get_question_detail(question_id: int, db: Session = Depends(get_db)):
     question = question_service.get_question(db, question_id=question_id)
     return question
+
+
+@router.post("/create",status_code= status.HTTP_204_NO_CONTENT)
+def create_question(_question_create: question_schema.QuestionCreate,
+                    db: Session = Depends(get_db)):
+    question_service.create_question(db, question_create=_question_create)
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,10 +2,13 @@
   import Router from 'svelte-spa-router'
   import Home from "./routes/Home.svelte"
   import Detail from "./routes/Detail.svelte"
+  // @ts-ignore
+  import QuestionCreate from "./routes/QuestionCreate.svelte"
 
   const routes = {
     '/': Home,
     '/detail/:question_id': Detail,
+    '/question-create' : QuestionCreate,
   }
 </script>
 
